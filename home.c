ebf24cdbb5367e9ce26f277f98d495a302285e12
gili-julio <giliardoplay@gmail.com>
2023-12-04 20:06:25 -0300

    update: criar tabela

--- home.c
+++ home.c
@@ -1,63 +1,86 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NOME_LIMITE 50 // Definindo o limite máximo dos nomes para 50
 
 typedef struct {
-    char nome[50];
+    char nome[NOME_LIMITE];
     int tipo;
 } Coluna;
 
 typedef struct {
-    char nome[50];
+    char nome[NOME_LIMITE];
     int numColunas;
-    Coluna colunas[10];
-    int chavePrimaria;
+    Coluna *colunas;
+    char colunaChavePrimaria[NOME_LIMITE];
 } Tabela;
 
 void criarTabela(Tabela *tabela){
-    printf("INFORME O NOME DA TABELA \n");
-    scanf("%s", tabela->nome);
+    getchar();
+    printf("INFORME O NOME DA TABELA: ");
+    fgets(tabela->nome, NOME_LIMITE, stdin);
+    tabela->nome[strcspn(tabela->nome, "\n")] = '\0';
+
+    printf("INFORME O NOME DA COLUNA DA CHAVE PRIMARIA: ");
+    fgets(tabela->colunaChavePrimaria, NOME_LIMITE, stdin);
+    tabela->colunaChavePrimaria[strcspn(tabela->colunaChavePrimaria, "\n")] = '\0';
     
-    printf("INFORME O NUMERO DE COLUNAS DA TABELA %s \n", tabela->nome);
-    scanf("%d", &(tabela->numColunas));
-    
-    for(int i = 0; i < tabela->numColunas; i++){
-        printf("INFORME O NOME DA COLUNA %d: \n", i+1);
-        scanf("%s", tabela->colunas[i].nome);
-        
-        int posTipo = 0;
-        while(posTipo == 0){
-            printf("INFORME O TIPO DA COLUNA %d: \n | 1 - char | 2 - int | 3 - float | 4 - double | 5 - string | \n", i+1);
-            scanf("%d", &posTipo);
-            switch (posTipo){
-                case 1:
-                case 2:
-                case 3:
-                case 4:
-                case 5:
-                    tabela->colunas[i].tipo = posTipo;
-                    break;
-                default:
-                    printf("INVALIDO! \n");
-                    posTipo = 0;
-                    break;
-            }
-        }
-    }
-    
-    int posPrimaria = 0;
-    while(posPrimaria == 0){
-        printf("INFORME QUAL A COLUNA DA CHAVE PRIMARIA (de 1 a %d): \n", tabela->numColunas);
-        scanf("%d", &posPrimaria);
-        if(posPrimaria >= 1 && posPrimaria <= tabela->numColunas){
-            tabela->chavePrimaria = posPrimaria;
-        } else {
-            printf("INVALIDO! \n");
-            posPrimaria = 0;
+    // Criação de colunas da tabela
+    int op;
+    tabela->numColunas = 0;
+    tabela->colunas = NULL;
+    while(op != 0){
+        printf("| 0 - Finalizar tabela | 1 - Adicionar coluna |\nINFORME O QUE DESEJA: ");
+        scanf("%d", &op);
+        getchar();
+        switch(op){
+            case 0:
+                break;
+            case 1:
+
+                // Alocação de memória para as colunas
+                tabela->colunas = realloc(tabela->colunas, (tabela->numColunas+1) * sizeof(Coluna));
+                if (tabela->colunas == NULL) {
+                    printf("ERRO AO ALOCAR MEMORIA PARA AS COLUNAS");
+                    exit(1);
+                }
+
+                printf("INFORME O NOME DA COLUNA: ");
+                fgets(tabela->colunas[tabela->numColunas].nome, NOME_LIMITE, stdin);
+                tabela->colunas[tabela->numColunas].nome[strcspn(tabela->colunas[tabela->numColunas].nome, "\n")] = '\0';
+
+                int posTipo = 0;
+                while(posTipo == 0){
+                    printf("| 1 - char | 2 - int | 3 - float | 4 - double | 5 - string |\nINFORME O TIPO DA COLUNA: ");
+                    scanf("%d", &posTipo);
+                    getchar();
+                    switch (posTipo){
+                        case 1:
+                        case 2:
+                        case 3:
+                        case 4:
+                        case 5:
+                            tabela->colunas[tabela->numColunas].tipo = posTipo;
+                            break;
+                        default:
+                            printf("INVALIDO!\n");
+                            posTipo = 0;
+                            break;
+                    }
+                }
+                tabela->numColunas++;
+
+                break;
+            default:
+                printf("INVALIDO!\n");
+                break;
         }
     }
     
     // Nome do arquivo baseado no nome da tabela
     FILE *arquivo;
-    char nomeArquivo[100];
+    char nomeArquivo[NOME_LIMITE+4];
     sprintf(nomeArquivo, "%s.txt", tabela->nome);
     
     // Abre o arquivo para escrita
@@ -65,14 +88,14 @@ void criarTabela(Tabela *tabela){
     
     // Verifica se o arquivo foi aberto com sucesso
     if(arquivo == NULL){
-        printf("ERRO AO ABRIR O ARQUIVO %s PARA ESCRITA \n", nomeArquivo);
+        printf("ERRO AO ABRIR O ARQUIVO %s PARA ESCRITA", nomeArquivo);
         return;
     }
     
     // Escreve os dados da tabela no arquivo
     fprintf(arquivo, "nome: %s\n", tabela->nome);
     fprintf(arquivo, "numeroColunas: %d\n", tabela->numColunas);
-    fprintf(arquivo, "chavePrimaria: %d\n", tabela->chavePrimaria);
+    fprintf(arquivo, "colunaChavePrimaria: %s\n", tabela->colunaChavePrimaria);
     for(int i = 0; i < tabela->numColunas; i++){
         fprintf(arquivo, "nomeColuna %d: %s\n", i+1, tabela->colunas[i].nome);
         fprintf(arquivo, "tipoColuna %d: %d\n", i+1, tabela->colunas[i].tipo);
@@ -108,6 +131,9 @@ void criarTabela(Tabela *tabela){
     printf("==============================\n");
     printf("= TABELA CRIADA COM SUCESSO! =\n");
     printf("==============================\n");
+
+    // Liberar memória alocada
+    free(tabela->colunas);
 }
 
 int main(){
